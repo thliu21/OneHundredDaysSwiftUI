e35feeae91cd0ecba97b9aab35b052d60bb85a1e
Tianhao Liu <arthurliu1994@gmail.com>
2022-11-09 21:09:31 +0800

    .gitignore is now working

--- WeSplit/Pods/Target Support Files/CurrencyText/CurrencyText-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CurrencyText : NSObject
-@end
-@implementation PodsDummy_CurrencyText
-@end
--- WeSplit/Pods/Target Support Files/Pods-WeSplit/Pods-WeSplit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeSplit : NSObject
-@end
-@implementation PodsDummy_Pods_WeSplit
-@end
