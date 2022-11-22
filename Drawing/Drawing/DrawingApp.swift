f5fdf375f1d495e29b91619a16760f07b0a13bc7
Tianhao Liu <arthurliu1994@gmail.com>
2022-11-22 12:25:20 -0800

    day 45

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Tianhao Liu on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TrapezoidView()
+        }
+    }
+}
--- Tryout/Tryout/ContentView.swift
+++ Tryout/Tryout/ContentView.swift
@@ -6,32 +6,45 @@
 //
 
 import SwiftUI
-import SwipeCell
 
-#if canImport(UIKit)
-extension View {
-   func hideKeyboard() {
-       UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-} }
-#endif
+class TestObject: ObservableObject {
+    var value: Int = 0 {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+}
 
 struct ContentView: View {
-    @State private var numbers = [5, 6, 7, 8]
+    @StateObject var data = TestObject()
     
     var body: some View {
-        List {
-            ForEach(Array(numbers.enumerated()), id: \.offset) { idx, num in
-                Text("\(num)")
-                    .swipeActions(edge: .trailing) {
-                        Button(role: .destructive, action: { remove(at: idx) } ) {
-                            Text("Delete")
-                        }
-                    }
+        NavigationView {
+            VStack {
+                Text("value is \(data.value)")
+                
+                NavigationLink {
+                    DetailView(data: data)
+                } label: {
+                    Text("Update")
+                }
             }
         }
     }
+}
+
+struct DetailView: View {
+    @ObservedObject var data: TestObject
     
-    private func remove(at index: Int) {
-        numbers.remove(at: index)
+    var body: some View {
+        VStack {
+            Text("current \(data.value)")
+            
+            Button {
+                data.value += 1
+            } label: {
+                Text("Plus!")
+            }
+        }
     }
 }
